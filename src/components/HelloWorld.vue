c5b4a6276099a0146cfa4d47622ffcc4867c21b9
Carry3033 <carry3033@qq.com>
2024-07-29 23:30:51 +0800

    feat: 父子组件间值传递示例

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <script setup>
 defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
+    msg: {
+        type: String,
+        required: true
+    }
 })
 
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 // 真值和空字符串会被视为 true
 const buttonDisabled = ref('')
 
@@ -14,44 +14,52 @@ let numberData = ref(1)
 const computedData = computed(() => numberData.value + 1)
 
 function addCount() {
-  return numberData.value++
+    return numberData.value++
 }
+
+// $ref 实例 比 ref 变量 优先级更高
+const input = ref(0)
+
+onMounted(() => {
+    input.value.value = '1'
+})
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-    <button :disabled="buttonDisabled">点不到</button>
-    <button @click="addCount">功德+{{ computedData }}</button>
-  </div>
+    <div class="greetings">
+        <h1 class="green">{{ msg }}</h1>
+        <h3>
+            You’ve successfully created a project with
+            <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+            <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+        </h3>
+        <button :disabled="buttonDisabled">点不到</button><br/>
+        <button @click="addCount">功德+{{ computedData }}</button><br/>
+        <input ref="input"/>
+    </div>
 </template>
 
 <style scoped>
 h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+    font-weight: 500;
+    font-size: 2.6rem;
+    position: relative;
+    top: -10px;
 }
 
 h3 {
-  font-size: 1.2rem;
+    font-size: 1.2rem;
 }
 
 .greetings h1,
 .greetings h3 {
-  text-align: center;
+    text-align: center;
 }
 
 @media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+    .greetings h1,
+    .greetings h3 {
+        text-align: left;
+    }
 }
 </style>
