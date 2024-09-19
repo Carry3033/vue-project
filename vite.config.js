a4bc35bddd8dc47ddf0134f8a9a065967f4d920c
Carry3033 <carry3033@qq.com>
2024-09-20 00:51:05 +0800

    feat: 增加@leetCode文件配置

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@leetCode': fileURLToPath(new URL('./leetCode', import.meta.url))
     }
   }
 })
