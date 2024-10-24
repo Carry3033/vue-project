a35dd1af2ae7f93152409b0c1bfb0a72f763114b
Carry3033 <carry3033@qq.com>
2024-10-24 22:30:32 +0800

    feat: 安装sass依赖支持scss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,18 +10,22 @@ import { ref, computed, onMounted } from 'vue'
 // 真值和空字符串会被视为 true
 const buttonDisabled = ref('')
 
-let numberData = ref(1)
-const computedData = computed(() => numberData.value + 1)
+let meritCount = ref(1)
+const computedData = computed(() => meritCount.value + 1)
 
-function addCount() {
-    return numberData.value++
+function addMeritCount() {
+    meritCount.value++
 }
 
 // $ref 实例 比 ref 变量 优先级更高
 const input = ref(0)
 
+function addCount() {
+    input.value.value = (+input.value.value || 0) + computedData.value
+}
+
 onMounted(() => {
-    input.value.value = '1'
+    input.value.value = 1
 })
 
 defineExpose({
@@ -37,27 +41,36 @@ defineExpose({
             <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
             <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
         </h3>
-        <button :disabled="buttonDisabled">点不到</button><br/>
-        <button @click="addCount">功德+{{ computedData }}</button><br/>
-        <input ref="input"/>
+        <button :disabled="buttonDisabled">点不到</button>
+        <button @click="addMeritCount">功德+{{ computedData }}</button>
+        <button @click="addCount">累加</button>
+        <br />
+        <input ref="input" />
     </div>
 </template>
 
-<style scoped>
-h1 {
-    font-weight: 500;
-    font-size: 2.6rem;
-    position: relative;
-    top: -10px;
-}
+<style scoped lang="scss">
+.greetings {
+    h1 {
+        text-align: center;
+        font-weight: 500;
+        font-size: 2.6rem;
+        position: relative;
+        top: -10px;
+    }
 
-h3 {
-    font-size: 1.2rem;
-}
+    h3 {
+        text-align: center;
+        font-size: 1.2rem;
+    }
 
-.greetings h1,
-.greetings h3 {
-    text-align: center;
+    button {
+        margin: 8px 0;
+
+        + button {
+            margin-left: 8px;
+        }
+    }
 }
 
 @media (min-width: 1024px) {
