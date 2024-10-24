6d06ada6235519a4be31efc497a710ecf3e08d8e
Carry3033 <carry3033@qq.com>
2024-10-24 23:09:30 +0800

    refactor: 使用配置形式渲染组件

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -6,32 +6,45 @@ import EcosystemIcon from '@/components/icons/IconEcosystem.vue'
 import CommunityIcon from '@/components/icons/IconCommunity.vue'
 import SupportIcon from '@/components/icons/IconSupport.vue'
 import LeetCode2414 from '@leetCode/2414.vue'
+import { shallowRef } from 'vue'
+
+/**
+ *  当使用 ref 时会有以下警告，因为响应式耗性能
+ *  [Vue warn]: Vue received a Component that was made a reactive object.
+ *  This can lead to unnecessary performance overhead and should be avoided by
+ *  marking the component with `markRaw` or using `shallowRef` instead of `ref`.
+ */
+const welcomeItemList = shallowRef([
+    {
+        heading: 'LeetCode',
+        iconComponent: DocumentationIcon, // 直接绑定到组件本身，而不是字符串的组件名
+        defaultComponent: LeetCode2414
+    },
+    {
+        heading: 'Style',
+        iconComponent: ToolingIcon
+    },
+    {
+        heading: 'Components',
+        iconComponent: EcosystemIcon
+    },
+    {
+        heading: 'Utils',
+        iconComponent: CommunityIcon
+    },
+    {
+        heading: 'Business',
+        iconComponent: SupportIcon
+    }
+])
 </script>
 
 <template>
-    <WelcomeItem>
-        <template #icon><DocumentationIcon /></template>
-        <template #heading>LeetCode</template>
-        <LeetCode2414 />
-    </WelcomeItem>
-
-    <WelcomeItem>
-        <template #icon><ToolingIcon /></template>
-        <template #heading>Style</template>
-    </WelcomeItem>
-
-    <WelcomeItem>
-        <template #icon><EcosystemIcon /></template>
-        <template #heading>Components</template>
-    </WelcomeItem>
-
-    <WelcomeItem>
-        <template #icon><CommunityIcon /></template>
-        <template #heading>Utils</template>
-    </WelcomeItem>
-
-    <WelcomeItem>
-        <template #icon><SupportIcon /></template>
-        <template #heading>Business</template>
+    <WelcomeItem v-for="item in welcomeItemList" :key="item.heading">
+        <template #icon>
+            <component :is="item.iconComponent"></component>
+        </template>
+        <template #heading>{{ item.heading }}</template>
+        <component :is="item.defaultComponent"></component>
     </WelcomeItem>
 </template>
